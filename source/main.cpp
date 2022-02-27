553755b9cea8d7c9d1dd8b43a94335fa53084c91
Robin Southern <betajaen@users.noreply.github.com>
2022-02-27 09:17:06 +0000

    Newer startup code for string pools

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,7 @@
 #include <proto/dos.h>
 
 #include "game.h"
+#include "crt.h"
 
 struct Library* CyberGfxBase = NULL;
 
@@ -27,15 +28,20 @@ static void libs_cleanup()
 
 int main(int argc, char** argv)
 {
+	using namespace Mohawk;
+
 	if (libs_init())
 	{
-		Mohawk::Game game;
+		AllocateSharedStringMemory();
+		
+		Game game;
 
 		if (game.Setup())
 		{
 			game.Run();
 		}
 
+		ReleaseSharedStringMemory();
 	}
 
 	libs_cleanup();
